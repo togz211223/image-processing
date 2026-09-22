f8abaa5b47cedcc3f0475164c4543c36e10eff50
togz211223 <mynewrealenemytog@gmail.com>
2026-09-22 17:34:30 +0300

    Update Template_Code.cpp

--- Template_Code.cpp
+++ Template_Code.cpp
@@ -180,6 +180,14 @@ Image flipHorizontal(const Image& input) {
     // TODO: Implement this function
     // For each pixel and each channel:
     //   output(y, width-1-x, c) = input(y, x, c)
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            for (int c = 0; c < channels; c++) {
+                output(y, width - 1 - x, c) = input(y, x, c);
+            }
+        }
+    }
+    
     
     return output;
 }
@@ -203,6 +211,14 @@ Image flipVertical(const Image& input) {
     // TODO: Implement this function
     // For each pixel and each channel:
     //   output(height-1-y, x, c) = input(y, x, c)
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            for (int c = 0; c < channels; c++) {
+        output(height - 1 - y, x, c) = input(y, x, c);
+        }
+    }
+        }
+    
     
     return output;
 }
